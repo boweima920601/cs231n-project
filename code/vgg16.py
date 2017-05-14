21184d6d6843adc72f135efa3dd695a2770b4963
Yun Nie <amodashikagome@gmail.com>
2017-05-13 23:25:42 -0700

    load_data

--- code/vgg16.py
+++ code/vgg16.py
@@ -2,6 +2,25 @@ import numpy as np
 import tensorflow as tf
 from tf.layers import conv2d, batch_normalization, max_pooling2d, dense, dropout
 from tf.nn import relu
+import math
+
+def load_data(data, label, debug = True):
+    X_train = np.load(data)
+    y_train = np.load(label)
+    if debug:
+        X_train = X_train[:1000,]
+        y_train = y_train[:1000,]
+    train_indicies = np.arange(X_train.shape[0])
+    np.random.shuffle(train_indicies)
+    num_training = math.ceil(X_train.shape[0] * 0.8)
+    in_train = train_indicies[:num_training]
+    in_val = train_indicies[num_training:]
+    X_train = X_train[in_train]
+    y_train = y_train[in_train]
+    X_val = X_train[in_val]
+    y_val = y_train[in_val]
+    return X_train, y_train, X_val, y_val
+
 
 def vgg16(X, y, is_training):
     conv1 = X
@@ -29,14 +48,13 @@ def vgg16(X, y, is_training):
         conv11 = conv2d(conv11, filter = 512, kernel_size = [3, 3], padding = 'same', activation = relu)
     pool5 = max_pooling2d(conv11, pool_size = [2, 2], strides = 2)
 
-    pool5_flat = tf.reshape(pool5, [-1, ?????])
-    fc1 = dense(pool5_flat, units=???, activation = relu)
-    fc2 = dense(fc1, units=???, activation = relu)
-    fc3 = dense(fc2, units=???, activation = relu)
+    pool5_flat = tf.reshape(pool5, [-1, 4096])
+    fc1 = dense(pool5_flat, units = 4096, activation = relu)
+    fc2 = dense(fc1, units = 4096, activation = relu)
 
     # dropout1 = dropout(fc3, rate=0.5, training=is_training)
     # logits = dense(dropout1, units=10)
-    logits = dense(fc3, units=10)
+    logits = dense(fc2, units = 10)
 
     return logits
 
@@ -106,12 +124,13 @@ def run_model(session, predict, loss_val, Xd, yd,
     return total_loss,total_correct
 
 
+
 tf.reset_default_graph()
 
-X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+X = tf.placeholder(tf.float32, [None, 224, 224, 3])
 y = tf.placeholder(tf.int64, [None])
 is_training = tf.placeholder(tf.bool)
-y_out = my_model(X,y,is_training)
+y_out = vgg16(X,y,is_training)
 
 total_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = y_out)
 mean_loss = tf.reduce_mean(total_loss)
@@ -119,15 +138,25 @@ mean_loss = tf.reduce_mean(total_loss)
 global_step = tf.Variable(0, trainable=False)
 starter_learning_rate = 1e-3
 learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
-                                           800, 0.9, staircase=True)
+                                           1000, 0.9, staircase=True)
 optimizer = tf.train.AdamOptimizer(learning_rate)
+train_step = optimizer.minimize(mean_loss, global_step=global_step)
 
 # batch normalization in tensorflow requires this extra dependency
-extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-with tf.control_dependencies(extra_update_ops):
-    train_step = optimizer.minimize(mean_loss, global_step=global_step)
+# extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+# with tf.control_dependencies(extra_update_ops):
+#     train_step = optimizer.minimize(mean_loss, global_step=global_step)
 
 
+X_train, y_train, X_val, y_val = load_data('train_data.npy', 'train_label.npy')
+
+
+"""
+run_model(session, predict, loss_val, Xd, yd,
+              epochs=1, batch_size=64, print_every=100,
+              training=None, plot_losses=False)
+"""
+              
 with tf.Session() as sess:
     with tf.device("/cpu:0"): #"/cpu:0" or "/gpu:0"
         sess.run(tf.global_variables_initializer())
