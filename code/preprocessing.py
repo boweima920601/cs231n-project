9c3c98e5b69ebdc7fa569bf1ea243d3b67f06716
boweima920601 <mbwxyz123@hotmail.com>
2017-05-13 21:14:55 -0700

    finished preprocessing.py

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -6,25 +6,99 @@ import cv2
 import matplotlib.pyplot as plt
 import sys
 from skimage import io, transform
+import os
+import glob
 
-PATH_TO_DATA = '../data'
 
-train = pd.read_csv(PATH_TO_DATA + '/driver_imgs_list.csv')
+# color_type = 1 - gray
+# color_type = 3 - RGB
+def get_im_cv2(path, img_cols, img_rows, color_type=1):
+    # Load as grayscale
+    if color_type == 1:
+        img = cv2.imread(path, 0)
+    elif color_type == 3:
+        img = cv2.imread(path)
+    # Reduce size
+    resized = cv2.resize(img, (img_cols, img_rows))
+    return resized
 
-train['id'] = range(train.shape[0])
-subj = np.array(train['subject'])
-num_subj = np.unique(train['subject'])
-imgs = []
 
-for index, row in train.iterrows():
-	img_path = PATH_TO_DATA + '/train/'+ str(row[1]) +'/'+ str(row[2])
-	img = cv2.imread(img_path)
-	img = cv2.resize(img, (160, 120))
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	imgs.append(img)
+def get_im_cv2_mod(path, img_cols, img_rows, color_type = 1):
+    # Load as grayscale
+    if color_type == 1:
+        img = cv2.imread(path, 0)
+    else:
+        img = cv2.imread(path)
+    # Reduce size
+    rotate = random.uniform(-10, 10)
+    M = cv2.getRotationMatrix2D((img.shape[1]/2, img.shape[0]/2), rotate, 1)
+    img = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+    resized = cv2.resize(img, (img_cols, img_rows), cv2.INTER_LINEAR)
+    return resized
 
-	if row['id'] % 100 == 0:
-		print('{} done'.format(row['id']))
+def load_train_data(PATH_TO_DATA, cols, rows, color):
 
-imgs = np.array(imgs)
-print(imgs.shape)
+	print("Load train images")
+	start_time = time.time()
+	train = pd.read_csv(PATH_TO_DATA + '/driver_imgs_list.csv')
+
+	num_train = train.shape[0]
+	train['id'] = range(num_train)
+
+	subj = np.array(train['subject'])
+	num_subj = np.unique(train['subject'])
+	imgs = []
+	labels = []
+
+	for index, row in train.iterrows():
+		img_path = PATH_TO_DATA + '/train/'+ str(row[1]) +'/'+ str(row[2])
+		img = get_im_cv2(img_path, cols, rows, color)
+		# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		imgs.append(img)
+
+		labels.append(int(list(str(row[1]))[-1]))
+		if (row['id'] * 10) % num_train == 0:
+			print('Load images {} done out of total {} images'.format(row['id'], num_train))
+
+    print('Read test data time: {} seconds'.format(round(time.time() - start_time, 2)))
+	imgs = np.array(imgs)
+	labels = np.array(labels)
+	return imgs, labels
+
+def load_test_data(img_rows, img_cols, color_type = 1):
+    print('Read test images')
+    start_time = time.time()
+    path = os.path.join('..', 'input', 'test', '*.jpg')
+    files = glob.glob(path)
+    X_test = []
+    X_test_id = []
+    total = 0
+    thr = math.floor(len(files)/10)
+    for fl in files:
+        flbase = os.path.basename(fl)
+        img = get_im_cv2_mod(fl, img_rows, img_cols, color_type)
+        X_test.append(img)
+        X_test_id.append(flbase)
+        total += 1
+        if total%thr == 0:
+            print('Read {} images from {}'.format(total, len(files)))
+    
+    print('Read test data time: {} seconds'.format(round(time.time() - start_time, 2)))
+    X_test = np.array(X_test)
+    return X_test, X_test_id
+
+if __name__ == __main__:
+
+	PATH_TO_DATA = '../data'
+	cols = 224
+	rows = 224
+	color = 3
+	train, labels, subj = load_train_data(PATH_TO_DATA, cols, rows, 3)
+
+	test, test_id = load_test_data(cols, rows, 3)
+
+	np.save('train_data', train)
+	np.save('train_label', labels)
+	np.save('train_subj', subj)
+	np.save('test_data', test)
+	np.save('test_data_id', test_id)
