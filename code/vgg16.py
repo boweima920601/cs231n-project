5a2f3bd179992499c5cb73c582b2ca1ffb7ab7ef
Yun Nie <amodashikagome@gmail.com>
2017-05-13 19:39:17 -0700

    run_model

--- code/vgg16.py
+++ code/vgg16.py
@@ -31,9 +31,105 @@ def vgg16(X, y, is_training):
     fc1 = dense(pool5_flat, units=???, activation = relu)
     fc2 = dense(fc1, units=???, activation = relu)
     fc3 = dense(fc2, units=???, activation = relu)
-    
+
     # dropout1 = dropout(fc3, rate=0.5, training=is_training)
     # logits = dense(dropout1, units=10)
     logits = dense(fc3, units=10)
 
     return logits
+
+
+def run_model(session, predict, loss_val, Xd, yd,
+              epochs=1, batch_size=64, print_every=100,
+              training=None, plot_losses=False):
+    # have tensorflow compute accuracy
+    correct_prediction = tf.equal(tf.argmax(predict,1), y)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # shuffle indicies
+    train_indicies = np.arange(Xd.shape[0])
+    np.random.shuffle(train_indicies)
+
+    training_now = training is not None
+
+    # setting up variables we want to compute (and optimizing)
+    # if we have a training function, add that to things we compute
+    variables = [mean_loss,correct_prediction,accuracy]
+    if training_now:
+        variables[-1] = training
+
+    # counter
+    iter_cnt = 0
+    for e in range(epochs):
+        # keep track of losses and accuracy
+        correct = 0
+        losses = []
+        # make sure we iterate over the dataset once
+        for i in range(int(math.ceil(Xd.shape[0]/batch_size))):
+            # generate indicies for the batch
+            start_idx = (i*batch_size)%X_train.shape[0]
+            idx = train_indicies[start_idx:start_idx+batch_size]
+
+            # create a feed dictionary for this batch
+            feed_dict = {X: Xd[idx,:],
+                         y: yd[idx],
+                         is_training: training_now }
+            # get batch size
+            actual_batch_size = yd[i:i+batch_size].shape[0]
+
+            # have tensorflow compute loss and correct predictions
+            # and (if given) perform a training step
+            loss, corr, _ = session.run(variables,feed_dict=feed_dict)
+
+            # aggregate performance stats
+            losses.append(loss*actual_batch_size)
+            correct += np.sum(corr)
+
+            # print every now and then
+            if training_now and (iter_cnt % print_every) == 0:
+                print("Iteration {0}: with minibatch training loss = {1:.3g} and accuracy of {2:.2g}"\
+                      .format(iter_cnt,loss,np.sum(corr)/actual_batch_size))
+            iter_cnt += 1
+        total_correct = correct/Xd.shape[0]
+        total_loss = np.sum(losses)/Xd.shape[0]
+        print("Epoch {2}, Overall loss = {0:.3g} and accuracy of {1:.3g}"\
+              .format(total_loss,total_correct,e+1))
+        if plot_losses:
+            plt.plot(losses)
+            plt.grid(True)
+            plt.title('Epoch {} Loss'.format(e+1))
+            plt.xlabel('minibatch number')
+            plt.ylabel('minibatch loss')
+            plt.show()
+    return total_loss,total_correct
+
+
+tf.reset_default_graph()
+
+X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+y = tf.placeholder(tf.int64, [None])
+is_training = tf.placeholder(tf.bool)
+y_out = my_model(X,y,is_training)
+
+total_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = y_out)
+mean_loss = tf.reduce_mean(total_loss)
+
+global_step = tf.Variable(0, trainable=False)
+starter_learning_rate = 1e-3
+learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
+                                           800, 0.9, staircase=True)
+optimizer = tf.train.AdamOptimizer(learning_rate)
+
+# batch normalization in tensorflow requires this extra dependency
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(extra_update_ops):
+    train_step = optimizer.minimize(mean_loss, global_step=global_step)
+
+
+with tf.Session() as sess:
+    with tf.device("/cpu:0"): #"/cpu:0" or "/gpu:0"
+        sess.run(tf.global_variables_initializer())
+        print('Training')
+        run_model(sess,y_out,mean_loss,X_train,y_train,1,64,100,train_step,True)
+        print('Validation')
+        run_model(sess,y_out,mean_loss,X_val,y_val,1,64)
